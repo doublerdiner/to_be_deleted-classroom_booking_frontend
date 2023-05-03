0b4eaef283f457a8a83bed8c92b4607c29b7bfe1
Chris Barclay <chris.barclay1@gmail.com>
2023-05-03 11:35:21 +0100

    First Commit

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,6 @@
+const HeaderBar = ()=>{
+    return(
+        <h1>Header</h1>
+    )
+}
+export default HeaderBar
---
+++ src/containers/ClassroomBooking.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import HeaderBar from "../components/HeaderBar"
+
+
+const ClassroomBooking = ()=>{
+
+return(
+    <Router>
+        <HeaderBar/>
+        <Routes>
+            <Route></Route>
+        </Routes>
+    </Router>
+)    
+}
+export default ClassroomBooking
